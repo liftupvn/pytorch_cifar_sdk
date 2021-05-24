2188c001e4a869e1f928ea5b4d9dc5720eb2725b
duccd <caodinhducuet@gmail.com>
2021-05-24 13:52:01 +0700

    change name of args

--- inference.py
+++ inference.py
@@ -5,8 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--name', default='Resnet 18', type=str, help='name of model')
 parser.add_argument('--username', default='seta dev', type=str, help='name of user')
-parser.add_argument('--ckpt_ids', default=None, type=str, help='ckpt_ids list')
-parser.add_argument('--dataset_tests', default=None, type=str, help='dataset_tests')
+parser.add_argument('--ckpt_id', default=None, type=str, help='ckpt_ids list')
+parser.add_argument('--dataset_test', default=None, type=str, help='dataset_tests')
 
 parser.add_argument('--accuracy', default=None, type=float, help='accuracy')
 parser.add_argument('--f1', default=None, type=float, help='f1')
@@ -33,7 +33,7 @@ print('benchmark: ', benchmark)
 # write test sumary
 MLOps.log.add_test_results(
     args.ckpt_id,
-    args.dataset_tests,
+    args.dataset_test,
     benchmark,
-    name=f'test model on {args.dataset_tests}'
+    name=f'test model on {args.dataset_test}'
     )
